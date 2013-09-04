e0ed85206a3e8a50bbb897bf562e6538a7e81f4f
Pawel Majewski <majo44@gmail.com>
2013-09-04 13:12:59 +0200

    Som style changes

--- slides/angular/directives/example/message.js
+++ slides/angular/directives/example/message.js
@@ -1,10 +1,8 @@
-alert("dupsdsdadsaa");
-
-angular.module("wk.lynx.site").directive("message", function() {
-    console.log("dsdasdsa");
-    return {
-        restrict : "E",
-        scope : { name: '=' },
-        templateUrl: "gitContents/slides/angular/directives/example/message.html"
-    }
-});
+angular.module("angularDirectivesExample", ["ng"])
+    .directive("message", function() {
+        return {
+            restrict : "E",
+            scope : { name: '=' },
+            templateUrl: "gitContents/slides/angular/directives/example/message.html"
+        }
+    });
