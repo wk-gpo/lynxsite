a655f20832f3d34f9fc2db92af1d0bc1115d9839
Pawel Majewski <majo44@gmail.com>
2013-09-09 16:24:19 +0200

    LynxSite

--- slides/framework/authentication/example/app.js
+++ slides/framework/authentication/example/app.js
@@ -1,10 +1,9 @@
 lynx.module({
     deferred: true,
-    moduleName: "modelsExample",
+    moduleName: "authExample",
     velvetConfig : {
         host: "http://velvet.cheetah.dev.lynxapp.net",
-        apikey: "5D2000D799874109A65DD0C984102DCC",
-        authMode: velvet.service.Auth.AUTH_RC4_TOKEN
+        apikey: "5D2000D799874109A65DD0C984102DCC"
     },
     globals: {
         "$auth" : { "factory": lynx.models.AuthModel.factory}
